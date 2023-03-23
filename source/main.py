3a831219c821be0dd7c51f2c8f22b1fca50852af
Wataru Nagashima <wataru.nagashima@ghelia.com>
2023-03-23 17:13:51 +0900

    AIきりたんを感情的に発話させるコードを出力した

--- source/main.py
+++ source/main.py
@@ -1,68 +1,60 @@
-import io
-import pyaudio
+import openai
 import requests
-from config import GCLOUD_KEY_PATH, VOICEVOX_API_URL, SPEAKER_ID
-from google.cloud import texttospeech, speech_v1p1beta1 as speech
-from google.oauth2 import service_account
-from gpt import chat_with_gpt
+import pyaudio
+from textblob import TextBlob
+import config
 
-credentials = service_account.Credentials.from_service_account_file(GCLOUD_KEY_PATH)
+def estimate_emotion(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    return "positive" if polarity > 0 else "negative"
 
-tts_client = texttospeech.TextToSpeechClient(credentials=credentials)
-stt_client = speech.SpeechClient(credentials=credentials)
+def chat_with_gpt(text):
+    model_engine = "text-davinci-002"
+    prompt = f"{text}\n\nChatGPT:"
 
-def record_audio():
-    audio_format = pyaudio.paInt16
-    sample_rate = 16000
-    channels = 1
-    chunk_size = 1024
-    audio = pyaudio.PyAudio()
-
-    stream = audio.open(format=audio_format,
-                        rate=sample_rate,
-                        channels=channels,
-                        input=True,
-                        frames_per_buffer=chunk_size)
-
-    print("録音を開始してください。")
-    frames = []
-
-    for _ in range(0, int(sample_rate / chunk_size * 5)):
-        data = stream.read(chunk_size)
-        frames.append(data)
-
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    return b''.join(frames)
-
-def speech_to_text(audio_data):
-    audio = speech.RecognitionAudio(content=audio_data)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code="ja-JP"
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7,
     )
 
-    response = stt_client.recognize(config=config, audio=audio)
-    return response.results[0].alternatives[0].transcript if response.results else ""
+    response_text = response.choices[0].text.strip()
+    return response_text
 
 def text_to_speech(text):
+    emotion = estimate_emotion(text)
+
+    speed_scale = 1.0
+    pitch_scale = 0.0
+    intonation_scale = 1.0
+
+    if emotion == "positive":
+        speed_scale = 1.2
+        pitch_scale = 0.5
+        intonation_scale = 1.2
+    elif emotion == "negative":
+        speed_scale = 0.9
+        pitch_scale = -0.5
+        intonation_scale = 0.8
+
     response = requests.post(
-        f"{VOICEVOX_API_URL}/audio_query",
+        f"{config.VOICEVOX_API_URL}/audio_query",
         json={
             "text": text,
-            "speaker": SPEAKER_ID,
-            "speedScale": 1.0,
-            "pitchScale": 0.0,
-            "intonationScale": 1.0,
+            "speaker": config.SPEAKER_ID,
+            "speedScale": speed_scale,
+            "pitchScale": pitch_scale,
+            "intonationScale": intonation_scale,
             "volumeScale": 1.0,
         },
     )
 
     audio_query = response.json()
-    response = requests.post(f"{VOICEVOX_API_URL}/synthesis", json=audio_query)
+    response = requests.post(f"{config.VOICEVOX_API_URL}/synthesis", json=audio_query)
     audio_data = response.content
 
     audio = pyaudio.PyAudio()
@@ -79,17 +71,9 @@ def text_to_speech(text):
     stream.close()
     audio.terminate()
 
-def main():
-    while True:
-        audio_data = record_audio()
-        text = speech_to_text(audio_data)
-        print(f"あなた: {text}")
-
-        response_text = chat_with_gpt(text)
-        print(f"ChatGPT: {response_text}")
-
-        text_to_speech(response_text)
-
 if __name__ == "__main__":
-    main()
-
+    while True:
+        text = input("あなた: ")
+        response_text = chat_with_gpt(text)
+        print("ChatGPT:", response_text)
+        text_to_speech(response_text)
